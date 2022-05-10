0958d7e7e1d7c7e149eaf2f430b9538981133dcc
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-05-10 17:22:24 +0300

    cycle "while" changed to cycle "for"

--- Guess The Number/Program.cs
+++ Guess The Number/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Guess_The_Number
 {
@@ -15,32 +16,35 @@ namespace Guess_The_Number
             lower = rand.Next(number - 10, number - 1);
             longer = rand.Next(number + 1, number + 10);
             
-            Console.WriteLine($"Мы загадали вам число. Оно больше {lower}, но меньше{longer}");
+            Console.WriteLine($"Мы загадали вам число. Оно больше {lower}, но меньше {longer}");
             Console.WriteLine($"У вас есть {tryCount} попыток.");
             Console.WriteLine("Что это за число?");
-            while (tryCount-- > 0)
+            
+            for (int i = 1; i <= tryCount; i++)
             {
+                Console.WriteLine($"\nПопытка №{i}");
                 Console.Write("Это число:");
                 userInput = Convert.ToInt32(Console.ReadLine());
 
                 if (userInput == number)
                 {
-                    Console.WriteLine("Поздравляю, вы угодали.");
+                    Console.WriteLine($"Поздравляю, вы смогли угодать с {i}-ой попытки.");
                     break;
                 }
-                else if(tryCount == 0)
+                else if(tryCount == i)
                 {
                     Console.WriteLine("Вы проиграли.");
+                    Console.WriteLine($"Было загадано чило {number}.");
                     break;
                 }
-                else if(tryCount == 1)
+                else if(i == tryCount - 1)
                 {
-                    Console.WriteLine($"Призовите на помощ всё своё везение, это последняя попытка.");
+                    Console.WriteLine($"Призовите на помощь всё своё везение, это последняя попытка.");
                 }
-                else 
+                else
                 {
                     Console.WriteLine("Вы не угодали, попробуйте еще раз.");
-                    Console.WriteLine($"Остальсь попыток: {tryCount}");
+                    Console.WriteLine($"Осталось попыток: {tryCount-i}");
                 }
             }
         }
